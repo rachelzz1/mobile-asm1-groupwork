a81b9d92068720c0fa1e69e3eab6eb4f8e7da6a4
rachelzz1 <rqztian041007@gmail.com>
2025-06-26 00:56:16 +0800

    修改数据库字段

--- lib/screens/user_details_screen.dart
+++ lib/screens/user_details_screen.dart
@@ -40,7 +40,7 @@ class _UserDetailsScreenState extends State<UserDetailsScreen> {
     final doc = await FirebaseFirestore.instance.collection('users').doc(widget.userId).get();
     final data = doc.data() ?? {};
     setState(() {
-      nameController.text = data['name'] ?? '';
+      nameController.text = data['username'] ?? ''; // 修改为 username
       genderController.text = data['gender'] ?? '';
       heightController.text = data['height']?.toString() ?? '';
       weightController.text = data['weight']?.toString() ?? '';
@@ -53,7 +53,7 @@ class _UserDetailsScreenState extends State<UserDetailsScreen> {
 
   Future<void> _saveUserData() async {
     await FirebaseFirestore.instance.collection('users').doc(widget.userId).update({
-      'name': nameController.text,
+      'username': nameController.text, // 修改为 username
       'gender': genderController.text,
       'height': heightController.text,
       'weight': weightController.text,
